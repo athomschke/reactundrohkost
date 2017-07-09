0be1a4d6fd715a91b73ea31749df8faf16455302
Cornelia Rehbein <github@connyrehbein.com>
2017-07-09 11:13:02 +0200

    use image mapper

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import baseImage from './react_and_rohkost_assets/images/vegetables2.jpg';
 
 import './App.css';
+import ImageMapper from './ImageAnalyzer';
 
 class App extends Component {
 	onImageLoad() {
@@ -11,13 +12,15 @@ class App extends Component {
 		canvas.height = baseImageElement.height;
 		const ctx = canvas.getContext('2d');
 		ctx.drawImage(baseImageElement, 0, 0);
+		var test = ctx.getImageData(50, 50, 100, 100)
+		ImageMapper(test)
 	}
 
 	render() {
 		return (
 			<div className="App">
-				<canvas ref="canvas" className="baseCanvas"/>
-				<img ref="baseImage" src={baseImage} className='BaseImage' alt="vegetables2" onLoad={this.onImageLoad.bind(this)}/>
+				<canvas ref="canvas" className="baseCanvas" />
+				<img ref="baseImage" src={baseImage} className='BaseImage' alt="vegetables2" onLoad={this.onImageLoad.bind(this)} />
 			</div>
 		);
 	}
