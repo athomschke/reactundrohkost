ea2fd1ed09cb7081029bee2b1c52195f7984c153
Astrid Thomschke <astrid.thomschke@gmail.com>
2017-07-09 10:13:29 +0200

    Switch React App to displaying only the gritter

---
+++ src/ImageGritter/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import baseImage from '../react_and_rohkost_assets/images/vegetables2.jpg';
+
+export default function ImageGritter (){
+	return <img src={baseImage} className="BaseImage" alt="vegetables2" />;
+}
