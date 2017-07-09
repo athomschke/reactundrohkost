c93049db14e1a2d06090f611cfa5d7f3fab3ba1f
Astrid Thomschke <astrid.thomschke@gmail.com>
2017-07-09 10:34:37 +0200

    remove default stuff from app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import baseImage from '../react_and_rohkost_assets/images/vegetables2.jpg';
 
-import ImageGritter from './ImageGritter'
 import './App.css';
 
 class App extends Component {
@@ -17,14 +17,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <img ref="baseImage" src={baseImage} className='BaseImage'/>
         <canvas ref="canvas" />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
