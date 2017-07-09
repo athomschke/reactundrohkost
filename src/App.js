273a5c121e32738cfe8fb448e85dd359dc19fc1c
Astrid Thomschke <astrid.thomschke@gmail.com>
2017-07-09 11:13:46 +0200

    extract image gritter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
 import baseImage from './react_and_rohkost_assets/images/vegetables2.jpg';
 
+import ImageGritter from './ImageGritter';
+
 import './App.css';
 import ImageMapper from './ImageAnalyzer';
 import apple from './react_and_rohkost_assets/icons/png/apple.png';
 
 class App extends Component {
-	onImageLoad() {
-		const baseImageElement = this.refs.baseImage;
-		const canvas = this.refs.canvas;
-		canvas.width = baseImageElement.width;
-		canvas.height = baseImageElement.height;
-		const ctx = canvas.getContext('2d');
-		ctx.drawImage(baseImageElement, 0, 0);
-
-		var test = ctx.getImageData(0, 0, 512, 512)
-		console.log("closest image", ImageMapper(test))
-	}
 
 	render() {
 		return (
 			<div className="App">
-				<canvas ref="canvas" className="baseCanvas" />
-				<img ref="baseImage" src={baseImage} className='BaseImage' alt="vegetables2" onLoad={this.onImageLoad.bind(this)} />
-
+				<ImageGritter />
 			</div>
 		);
 	}
--- src/ImageGritter/index.jsx
+++ src/ImageGritter/index.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 
 import baseImage from '../react_and_rohkost_assets/images/vegetables2.jpg';
 
-export default function ImageGritter (){
-	return <img src={baseImage} className="BaseImage" alt="vegetables2" />;
+class ImageGritter extends React.Component {
+	onImageLoad() {
+		const baseImageElement = this.refs.baseImage;
+		const canvas = this.refs.canvas;
+		canvas.width = baseImageElement.width;
+		canvas.height = baseImageElement.height;
+		const ctx = canvas.getContext('2d');
+		ctx.drawImage(baseImageElement, 0, 0);
+	}
+
+	render() {
+		return (
+			<div>
+				<canvas ref="canvas" className="baseCanvas" />
+				<img ref="baseImage" src={baseImage} className='BaseImage' alt="vegetables2" onLoad={this.onImageLoad.bind(this)}/>
+			</ div>
+		)
+	}
 }
+
+export default ImageGritter;
