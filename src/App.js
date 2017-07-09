02177529dd83eb826af965d82c31b3baead4d332
Astrid Thomschke <astrid.thomschke@gmail.com>
2017-07-09 11:06:02 +0200

    draw vegetables2 onto canvas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import baseImage from '../react_and_rohkost_assets/images/vegetables2.jpg';
+import baseImage from './react_and_rohkost_assets/images/vegetables2.jpg';
 
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    var canvas = this.refs.canvas;
-    var ctx = canvas.getContext('2d');
-    ctx.rect(10, 10, 100, 100);
-    ctx.fillStyle="#FF0000";
-    ctx.fill();
-    var test = ctx.getImageData(50, 50, 100, 100)
-    debugger;
+	onImageLoad() {
+		const baseImageElement = this.refs.baseImage;
+		const canvas = this.refs.canvas;
+		canvas.width = baseImageElement.width;
+		canvas.height = baseImageElement.height;
+		const ctx = canvas.getContext('2d');
+		ctx.drawImage(baseImageElement, 0, 0);
+	}
 
-  }
-  render() {
-    return (
-      <div className="App">
-        <img ref="baseImage" src={baseImage} className='BaseImage'/>
-        <canvas ref="canvas" />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<canvas ref="canvas" className="baseCanvas"/>
+				<img ref="baseImage" src={baseImage} className='BaseImage' alt="vegetables2" onLoad={this.onImageLoad.bind(this)}/>
+			</div>
+		);
+	}
 }
 
 export default App;
