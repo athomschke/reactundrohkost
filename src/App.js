c86c0061198fb68d59c8945f54ed4db6b0fd3a62
Cornelia Rehbein <github@connyrehbein.com>
2017-07-09 15:58:03 +0200

    remove color scale

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 import ImageGritter from './ImageGritter';
 
 import './App.css';
-import ImageScale from './ImageScale';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<ImageScale />
 				<ImageGritter />
 			</ div>
 		);
