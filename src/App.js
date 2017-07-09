4938e5d113eecd92b1c6720509f7ff16ada2b93b
Cornelia Rehbein <cornelia.rehbein@valsight.com>
2017-07-09 10:23:30 +0200

    render red canvas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    var canvas = this.refs.canvas;
+    var ctx = canvas.getContext('2d');
+    ctx.rect(10, 10, 100, 100);
+    ctx.fillStyle="#FF0000";
+    ctx.fill();
+    var test = ctx.getImageData(50, 50, 100, 100)
+    debugger;
+
+  }
   render() {
     return (
       <div className="App">
@@ -10,6 +20,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <canvas ref="canvas" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
