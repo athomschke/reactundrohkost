bca4dd707301b8c0366816cb9ab55bfb80b967c5
Astrid Thomschke <astrid.thomschke@gmail.com>
2017-07-09 12:01:11 +0200

    split image in blocks

--- src/ImageGritter/index.jsx
+++ src/ImageGritter/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import baseImage from '../react_and_rohkost_assets/images/vegetables2.jpg';
 
+const BOX_WIDTH = 512;
+
 class ImageGritter extends React.Component {
 	onImageLoad() {
 		const baseImageElement = this.refs.baseImage;
@@ -10,6 +12,19 @@ class ImageGritter extends React.Component {
 		canvas.height = baseImageElement.height;
 		const ctx = canvas.getContext('2d');
 		ctx.drawImage(baseImageElement, 0, 0);
+		const imageDatas = [];
+		for (let i = 0; i <= canvas.width - BOX_WIDTH; i += BOX_WIDTH ) {
+			for (let j = 0; j <= canvas.width - BOX_WIDTH; j += BOX_WIDTH ) {
+				imageDatas.push(ctx.getImageData(i,j,512,512));
+			}
+		}
+		imageDatas.forEach((imageData) => {
+			const imageCanvas = document.createElement('canvas');
+			imageCanvas.width = BOX_WIDTH;
+			imageCanvas.height = BOX_WIDTH;
+			imageCanvas.getContext('2d').putImageData(imageData, 0,0)
+			document.getElementsByClassName('App')[0].appendChild(imageCanvas);
+		})
 	}
 
 	render() {
